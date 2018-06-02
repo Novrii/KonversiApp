fa97aa2ea3223fa16ddefee2d7ae5aef6d635ec0
Novrii <38958308+Novrii@users.noreply.github.com>
2018-06-02 10:24:16 +0700

    commit

--- app/src/main/java/com/example/kid/konversiapp/MenuSejarah.java
+++ app/src/main/java/com/example/kid/konversiapp/MenuSejarah.java
@@ -2,12 +2,16 @@ package com.example.kid.konversiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MenuSejarah extends AppCompatActivity {
-
+    ImageView gambar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_sejarah);
+
+        gambar = (ImageView) findViewById(R.id.imageView);
+        gambar.setImageResource(R.drawable.man);
     }
 }
